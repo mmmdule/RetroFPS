4ff46b22f57d8a23d47f37efb45ae9a025d320b2
mmmdule <dulleyjedi@gmail.com>
2023-01-09 23:31:29 +0100

    BossImp class name fix

--- Assets/Scripts/BossImp.cs
+++ Assets/Scripts/BossImp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Imp : MonoBehaviour
+public class BossImp : MonoBehaviour
 {
     public NavMeshAgent agent;
 
