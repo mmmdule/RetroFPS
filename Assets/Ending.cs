f608485ab58a4bb5b04829d9b5545c001a34a25b
mmmdule <dulleyjedi@gmail.com>
2023-01-17 22:00:25 +0100

    Added Ending

---
+++ Assets/Ending.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Ending : MonoBehaviour
+{
+    // Start is called before the first frame update
+    char[] array;
+    [SerializeField]
+    float delay;
+    [SerializeField]
+    float repeatRate;
+    void Awake()
+    {
+        ReadEndingText();
+        array = endingText.ToCharArray();
+        InvokeRepeating(nameof(LetterByLetter), delay, repeatRate);
+    }
+    string endingText;
+    [SerializeField]
+    TextMeshProUGUI textMeshPro;
+    [SerializeField]
+    AudioSource audioSource;
+
+    int index = 0;
+    void LetterByLetter(){
+        if(index < array.Length){
+            textMeshPro.text += array[index++];
+            audioSource.enabled = index < array.Length;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(index >= array.Length && Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Space)){
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+    void ReadEndingText(){
+        using (StreamReader sr = new StreamReader(Application.streamingAssetsPath + "/EndText.txt")){
+            endingText = sr.ReadToEnd();
+            sr.Close();
+        }
+    }
+}
